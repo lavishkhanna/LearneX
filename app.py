60bf20d7b87e31419aaba62d7b4620558643f616
lavishkhanna <kavitakhanna75@gmail.com>
2024-10-05 23:23:17 +0530

    Adding list of concepts for subjects

--- app.py
+++ app.py
@@ -2,16 +2,41 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Homepage route
+# Sample course topics
+course_topics = {
+    'data_science': [
+        'Python Basics',
+        'Data Structures',
+        'Pandas and Numpy',
+        'Data Visualization',
+        'Machine Learning Introduction',
+    ],
+    'machine_learning': [
+        'Supervised Learning',
+        'Linear Regression',
+        'Classification Algorithms',
+        'Clustering',
+        'Neural Networks',
+    ],
+    'web_development': [
+        'HTML & CSS',
+        'JavaScript Basics',
+        'Responsive Design',
+        'Back-end with Flask/Django',
+        'Deploying Web Applications',
+    ]
+}
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# Route to handle the selected course
+# Route to handle course selection and display topics
 @app.route('/select_course', methods=['POST'])
 def select_course():
     selected_course = request.form['course']
-    return f"You selected {selected_course}!"
+    topics = course_topics.get(selected_course, [])
+    return render_template('course_topics.html', course=selected_course, topics=topics)
 
 if __name__ == "__main__":
     app.run(debug=True)
